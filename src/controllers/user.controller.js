47dc510d67dc7ee19e9b971b8b3486df06a05073
Akash Sapkal <akashsapkal7620@gmail.com>
2025-04-03 18:51:40 +0530

    add endpoint to recreate accessToken from refreshToken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { User } from "../models/user.models.js";
-
+import jwt from "jsonwebtoken";
 const generateAccessAndRefreshToken = async (user) => {
   try {
     // const user = await User.findById(userId);
@@ -137,6 +137,7 @@ const loginUser = asyncHandler(async (req, res) => {
     sameSite: "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000
   };
+
   return res
     .status(200)
     .cookie("accessToken", accessToken, options)
@@ -178,4 +179,40 @@ const logoutUser=asyncHandler(async (req, res)=>{
   .json(new ApiResponse(200,{},"User Logged Out Successfully"))
 });
 
-export { registerUser, loginUser, logoutUser }
+const refreshAccessToken=asyncHandler( async(req,res)=>{
+  const incomingRefreshToken=req.cookies?.refreshToken || req.body?.refreshToken;
+  if(!incomingRefreshToken){
+    throw new ApiError(401,"Token Not Available !!");
+  }
+  const userfromIncomingRefreshToken=jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+  const user=await User.findById(userfromIncomingRefreshToken?._id).select("-password");
+  if(!user || user.refreshToken !== incomingRefreshToken){
+    throw new ApiError(401,"Invalid RefreshToken !!");
+  }
+  const { accessToken, refreshToken }=await generateAccessAndRefreshToken(user);
+  
+  const options = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user,
+          accessToken,
+          refreshToken,
+        },
+        "User Logged In Successfully !"
+      )
+    );
+});
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken }
