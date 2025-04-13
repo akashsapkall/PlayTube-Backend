60e6c982d216aa0f2ce17344fd8cc44a1d00c001
Akash Sapkal <akashsapkal7620@gmail.com>
2025-04-13 19:07:03 +0530

    playlist model fixed

---
+++ src/models/playlist.models.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+// import aggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const playlistSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    videos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+// commentSchema.plugin(aggregatePaginate);
+export const Playlist = mongoose.model("Playlist", playlistSchema);
