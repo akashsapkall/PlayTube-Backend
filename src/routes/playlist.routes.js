fd5e0f7bb353a8df38d0ad19e14bf7d2fca4079a
Akash Sapkal <akashsapkal7620@gmail.com>
2025-04-22 23:10:27 +0530

    add palylist routes

---
+++ src/routes/playlist.routes.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import {
+  createPlaylist,
+  getUserPlaylists,
+  getPlaylistById,
+  addVideoToPlaylist,
+  removeVideoFromPlaylist,
+  deletePlaylist,
+  updatePlaylist,
+} from "../controllers/playlist.controller.js";
+
+import { verifyToken } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.post("/create-playlist", verifyToken, createPlaylist);
+router.get("/get-playlists", verifyToken, getUserPlaylists);
+router.get("/get-playlist/:playlistId", verifyToken, getPlaylistById);
+router.delete("/delete-playlist/:playlistId", verifyToken, deletePlaylist);
+router.patch("/update-playlist/:playlistId", verifyToken, updatePlaylist);
+router.patch("/add-video-to-playlist/:videoId/:playlistId", verifyToken, addVideoToPlaylist);
+router.patch("/remove-video-from-playlist/:videoId/:playlistId", verifyToken, removeVideoFromPlaylist);
+
+export default router;
